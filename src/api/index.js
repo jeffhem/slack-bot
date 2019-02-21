e34d278cd8befc55988b532774508b533406481e
Jeff Ng <ctng@us.ibm.com>
2019-02-21 11:45:06 -0600

    added commands for adding and swapping (#7)

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,7 @@ import { getDb, updateDb } from '../db';
 
 export default ({ config, db }) => {
   let api = Router();
-  
+
   // expose the breakfast endpoint
   api.post('/thebrain', (req, res) => {
     const { body } = req;
@@ -23,20 +23,49 @@ export default ({ config, db }) => {
       if (payload.text.includes('breakfast')) {
         getDb(db, 'breakfast').then(data => {
           let currentData = data;
-          console.log(currentData);
-          if (new Date() - new Date(currentData.lastDate) > 86400000) {
-            currentData = Object.assign(currentData, {
-              lastIndex: util.nextIndex(currentData),
-              lastDate: util.nextWed(),
-            });
-            updateDb(db, currentData);
-          }
+          const command = payload.text.split(':')[1];
+          let response = '';
           const date = new Date(currentData.lastDate).toDateString();
-          const response = `it's ${currentData.team[data.lastIndex]}'s turn on ${date}`;
-          console.log(response);
+
+          switch (command) {
+            case 'add':
+              if (payload.user === 'W4CEKPG4W') {
+                const newUser = payload.text.split(':')[2];
+                currentData.team.push(newUser);
+                updateDb(db, currentData);
+                response = `All set! ${newUser} is added to the end of the list.`;
+              } else {
+                response = 'Excuse me! Do I know you?'
+              }
+              break;
+
+            case 'swapNext':
+              if (payload.user === 'W4CEKPG4W') {
+                const currentIndex = currentData.lastIndex;
+                const nextIndex = util.nextIndex(currentData);
+                const tempNext = currentData.team[nextIndex];
+                currentData.team[nextIndex] = currentData.team[currentIndex];
+                currentData.team[currentIndex] = tempNext;
+                updateDb(db, currentData);
+                response = `Done! Swapped ${currentData.team[nextIndex]} with ${currentData.team[currentIndex]}`;
+              } else {
+                response = 'Nope.'
+              }
+              break;
+
+            default:
+              if (new Date() - new Date(currentData.lastDate) > 86400000) {
+                currentData = Object.assign(currentData, {
+                  lastIndex: util.nextIndex(currentData),
+                  lastDate: util.nextWed(),
+                });
+                updateDb(db, currentData);
+              }
+              response = `it's ${currentData.team[data.lastIndex]}'s turn on ${date}`;
+          }
           util.postMessage(response, payload.channel);
         })
-      }else if (payload.text.includes('lunch')) {
+      } else if (payload.text.includes('lunch')) {
         getDb(db, 'lunch').then(data => {
           util.postMessage(util.randomMsg(data.res), payload.channel);
         })
